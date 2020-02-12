902de6895d4a82022152007f66f52dd89d4397ee
PhilippeRenevierGonin <philippe.renevier@unice.fr>
2020-02-12 15:15:03 +0100

    réalisation du test du bouton, appuie 10 fois, vérification uniquement à fin

--- DemoPLPL/app/src/androidTest/java/com/example/demoplpl/MainActivityTest.java
+++ DemoPLPL/app/src/androidTest/java/com/example/demoplpl/MainActivityTest.java
@@ -5,12 +5,20 @@ import android.content.Intent;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import com.example.demoplpl.reseau.Connexion;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import java.util.concurrent.TimeUnit;
+
+import constantes.NET;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -18,12 +26,18 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doAnswer;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
+    int cpt = 0;
+
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class, true, false);
+
+    @Mock
+    Connexion connexion;
 
     @Before
     public void initConnexion() {
@@ -35,19 +49,49 @@ public class MainActivityTest {
         startIntent.putExtra(MainActivity.AUTOCONNECT, false);
         mActivityRule.launchActivity(startIntent);
 
+
+        MockitoAnnotations.initMocks(this);
+
+        mActivityRule.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mActivityRule.getActivity().setConnexion(connexion);
+                mActivityRule.getActivity().initVue();
+            }
+        });
+
+
+
     }
 
 
 
     @Test
     public void mettreÀJourLAffichageDuCompteur() {
-        onView(withId(R.id.button)).perform(click());
 
-        onView(withId(R.id.text)).check(matches(withText("20")));
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                cpt++;
+                mActivityRule.getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mActivityRule.getActivity().mettreÀJourLAffichageDuCompteur(cpt);
+                    }
+                });
+                return null;
+            }
+        }).when(connexion).envoyer(NET.AJOUTER);
+
+        onView(withId(R.id.text)).check(matches(withText("Hello World!")));
+
+        for(int i = 1; i< 11; i++) {
+            onView(withId(R.id.button)).perform(click());
         }
+
+        onView(withId(R.id.text)).check(matches(withText("10")));
+
+
+
     }
 }
